5a24876e779198ab270ad8b12a6d869fa0b5aa8a
Dima Kostenko <dima.kostenko.97@gmail.com>
2019-01-24 21:04:46 +0200

    First commit
    Залил проект на GitHub

---
+++ Bank_w_new/Assets/Scripts/ShopButtonsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopButtonsScript : MonoBehaviour {
+
+	public GameObject canvasObj;
+	Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = canvasObj.GetComponent<Animation> ();
+	}
+
+	public void ShopOpen(){
+		anim.Play ("Coin_shop_open");
+	}
+
+	public void ShopClose(){
+		anim.Play ("Coin_shop_close");
+	}
+}
