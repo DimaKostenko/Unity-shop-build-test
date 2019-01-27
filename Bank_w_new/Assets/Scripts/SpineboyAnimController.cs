84210570ef3cb5a92ce89305613030749f1d9d05
Dima Kostenko <dima.kostenko.97@gmail.com>
2019-01-28 00:20:12 +0200

    Spine add
    + spineboy animation.

---
+++ Bank_w_new/Assets/Scripts/SpineboyAnimController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class SpineboyAnimController : MonoBehaviour {
+
+	SkeletonGraphic skeletonAnimation;
+
+	// Use this for initialization
+	void Start () {
+		skeletonAnimation = GetComponent<SkeletonGraphic> ();
+	}
+	
+	// Update is called once per frame
+	public void GunTossAnim () {
+		skeletonAnimation.AnimationState.SetAnimation (0, "gun toss", true);
+	}
+}
